e850a72919bf78410e00de24c25dfec26c2b7e81
jorgem617 <102376144+jorgem617@users.noreply.github.com>
2026-06-18 15:49:00 -0500

    Remove sitemap integration from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://jorgecms.netlify.app',
-  integrations: [
-    sitemap({
-      filter: (page) => true,
-    }),
-  ],
 });
